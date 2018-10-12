120f35b57fee3da225b60fa0ed2b35e15e3574d5
Datoclement <70664914@qq.com>
2018-10-12 21:08:12 +0800

    condition on encrypted text

--- encryptedcom.sh
+++ encryptedcom.sh
@@ -1,6 +1,11 @@
 clear
 clear
 
+mkdir -p alice
+mkdir -p bob
+
+SHOW=1
+
 # generate private keys and public keys
 echo "Generating private keys and public keys..."
 openssl genrsa -out alice/privateA
@@ -17,7 +22,7 @@ echo
 
 # create a message from Alice
 msg="Hello Bob"
-echo "Alice created a message $msg"
+echo "Alice creates a message $msg"
 echo $msg > alice/msg.txt
 echo
 
@@ -25,35 +30,50 @@ echo
 echo "Alice encoding message..."
 echo
 
-echo "Firstly, she creates the keysim."
+echo "Alice creates the keysim."
 openssl rand -out alice/keysim 16
+if [ $SHOW = 0 ]
+then
 echo "keysim generated is:"
 cat alice/keysim
+fi
 echo
 
-echo "Then, she encodes the message into c1.txt"
+echo "Alice encodes the message into c1.txt"
 openssl enc -e -in alice/msg.txt -out alice/c1.txt -kfile alice/keysim -aes-128-cbc -p
+if [ $SHOW = 0 ]
+then
 echo "The encoded message is:"
 cat alice/c1.txt
+fi
 echo
 
-echo "Then, she encodes the keysim with bob's public key into c2.txt"
+echo "Alice encodes the keysim with bob's public key into c2.txt"
 openssl rsautl -encrypt -in alice/keysim -out alice/c2.txt -pubin -inkey alice/publicB
+if [ $SHOW = 0 ]
+then
 echo "The encoded keysim is:"
 cat alice/c2.txt
+fi
 echo
 
-echo "Then, she signs on the hashed keysim with her own private key into c3.txt"
+echo "Alice signs on the hashed keysim with her own private key into c3.txt"
 openssl md5 alice/keysim > alice/hkeysim
 openssl rsautl -sign -in alice/hkeysim -out alice/c3.txt -inkey alice/privateA
+if [ $SHOW = 0 ]
+then
 echo "The signed keysim is:"
 cat alice/c3.txt
+fi
 echo
 
-echo "Then, she sends c1.txt, c2.txt and c3.txt to Bob"
+echo "Alice sends c1.txt, c2.txt and c3.txt to Bob"
 cp alice/*.txt bob/
+if [ $SHOW = 0 ]
+then
 echo "Bob receives files as follows:"
 ls bob | grep ".txt"
+fi
 echo
 
 # Bob decodes the message and verifies
@@ -62,30 +82,33 @@ echo
 
 echo "Bob decodes c2.txt with his private key for the keysim"
 openssl rsautl -decrypt -in bob/c2.txt -out bob/keysim -inkey bob/privateB
+if [ $SHOW = 0 ]
+then
 echo "He finds the keysim is:"
 cat bob/keysim
+fi
 echo
 
-echo "Then, he decodes c1.txt with the keysim for the message"
+echo "Bob decodes c1.txt with the keysim for the message"
 openssl enc -d -in bob/c1.txt -out bob/msg.txt -kfile bob/keysim -aes-128-cbc -p
 echo "He finds the message be:"
 cat bob/msg.txt
 echo
 
-echo "For verification, he then hashes the keysim he obtained into hkeysim"
+echo "Bob then hashes the keysim he obtained into hkeysim"
 openssl md5 bob/keysim | cut -f 2 -d= > bob/hkeysim
 echo "The hashed keysim is:"
 cat bob/hkeysim
 echo
 
-echo "He unsigns the c3.txt using the public key of Alice into vhkeysim"
+echo "Bob unsigns the c3.txt using the public key of Alice into vhkeysim"
 openssl rsautl -verify -in bob/c3.txt -pubin -inkey bob/publicA | cut -f 2 -d= > bob/vhkeysim
 echo "The verification keysim hash code is:"
 cat bob/vhkeysim
 echo
 
-echo "He then compares hkeysim and vhkeysim"
+echo "Bob then compares hkeysim and vhkeysim"
 DIFF=`diff bob/hkeysim bob/vhkeysim | wc -l`
 diff bob/hkeysim bob/vhkeysim
-echo "He finds $DIFF place(s) different."
+echo "Bob finds $DIFF place(s) different."
 echo
